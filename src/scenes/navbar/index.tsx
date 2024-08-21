050a03d88c4b66d2b6fa963f3cdb9fe44ccf1e02
Jessica Olivares <rosewell@live.cl>
2024-08-21 17:08:43 +0200

    added enums to navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
-    selectedPage: string;
-    setSelectedPage: (value: string) => void;
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
 }
 
-const Navbar = (selectedPage, setSelectedPage: Props) => {
+const Navbar = ( {selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
 
   return (
@@ -19,34 +23,43 @@ const Navbar = (selectedPage, setSelectedPage: Props) => {
                 {/* LEFT SIDE */}
                 <img src={Logo} alt="logo" />
                 {/* RIGHT SIDE */}
-                <div className={`${flexBetween} w-full`}>
-                    {/* INNER LINKS */}
-                    <div className={`${flexBetween} gap-8 text-sm`}>
-                        <Link 
-                            page="Home" 
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}/>
-                        <Link 
-                            page="Benefits" 
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}/>
-                        <Link 
-                            page="Our Classes" 
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}/>
-                        <Link 
-                            page="Contact Us" 
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}/>
-                    </div>
+                { isAboveMediumScreens ? (
+                    <div className={`${flexBetween} w-full`}>
+                        {/* INNER LINKS */}
+                        <div className={`${flexBetween} gap-8 text-sm`}>
+                            <Link 
+                                page="Home" 
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}/>
+                            <Link 
+                                page="Benefits" 
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}/>
+                            <Link 
+                                page="Our Classes" 
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}/>
+                            <Link 
+                                page="Contact Us" 
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}/>
+                        </div>
 
-                    {/* MEMBER BUTTONS */}
-                    <div className={`${flexBetween} gap-8`}>
-                        <button>Sign In</button>
-                        <button>Become a Member</button>
+                        {/* MEMBER BUTTONS */}
+                        <div className={`${flexBetween} gap-8`}>
+                            <button>Sign In</button>
+                            <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
+                        </div>
                     </div>
-                </div>
-            </div>
+                ) : 
+                (
+                    <button className="rounded-full bg-secondary-500 p-2"
+                    onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                        <Bars3Icon className="text-white h-6 w-6"/>
+                    </button>
+                )
+                }
+            </div> 
         </div>
     </nav>
   )
