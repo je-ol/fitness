533258bc0eeb20eab7a0af1b36b0f607518e8e90
Jessica Olivares <rosewell@live.cl>
2024-08-26 17:55:38 +0200

    added framer motion animation and setSelectedPage fuctionality to header

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,8 @@ import SponsorRedBull from '@/assets/SponsorRedBull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
+
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -22,7 +24,16 @@ const Home = ({ setSelectedPage }: Props) => {
             {/* Main header*/}
             <div className='z-10 mt-32 md:basis-3/5'>
                 {/* Header */}
-                <div className='md:mt-20'>
+                <motion.div 
+                  className='md:mt-20'
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5}}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}>
                     <div className='relative'>
                         <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>                        
                             <img src={HomePageText} alt="home-page-text" />
@@ -33,9 +44,18 @@ const Home = ({ setSelectedPage }: Props) => {
                     Studios to get the Body Shapes That you Dream of.. Get Your Dream
                     Body Now.
                     </p>
-                </div>
+                </motion.div>
                 {/* Actions */}
-                <div className='mt-8 flex items-center gap-8'>
+                <motion.div 
+                  className='mt-8 flex items-center gap-8'
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5}}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}>
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -45,12 +65,13 @@ const Home = ({ setSelectedPage }: Props) => {
                     href={`#${SelectedPage.ContactUs}`}>
                         <p>Learn More</p>
                     </AnchorLink>
-                </div>
+                </motion.div>
             </div>
             {/* Image */}
-            <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
+            <motion.div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'
+              onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
                 <img src={HomePageGraphic} alt="home-page-graphic" />
-            </div>
+            </motion.div>
         </div>
         {/* SPONSORS */}
         {isAboveMediumScreens && (
